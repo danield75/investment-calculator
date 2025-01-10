b109f16a4ce6a9ed36ebf90f993b3ca2e97d1ec4
Daniel Drion <daniel.drion@telenet.be>
2025-01-10 16:03:49 +0100

    Added investment-results component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserInputComponent } from "./user-input/user-input.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserInputComponent],
-    bootstrap: [AppComponent],
-    imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserInputComponent,
+    InvestmentResultsComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule],
 })
 export class AppModule {}
---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: false,
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {}
